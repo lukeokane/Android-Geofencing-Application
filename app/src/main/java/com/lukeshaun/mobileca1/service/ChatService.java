e36bc5eac5ea85056864c675ca1d77857d848445
Luke <lukecjokane@gmail.com>
2019-04-30 00:32:14 +0100

    Basic remote service

---
+++ app/src/main/java/com/lukeshaun/mobileca1/service/ChatService.java
@@ -0,0 +1,156 @@
+package com.lukeshaun.mobileca1.service;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.support.v4.app.NotificationCompat;
+import android.widget.Toast;
+
+import com.lukeshaun.mobileca1.MapsActivity;
+import com.lukeshaun.mobileca1.R;
+
+import java.util.ArrayList;
+
+public class ChatService extends Service {
+    // Chat Service notification
+    NotificationManager mNM;
+    // Clients connected
+    ArrayList<Messenger> mClients = new ArrayList<>();
+    /** Holds last value set by a client. */
+    int mValue = 0;
+
+    // Allows inter process communication (IPC).
+    // Clients target this Messenger to communicate with service.
+    final Messenger mMessenger = new Messenger(new IncomingHandler());
+
+
+    // Command to the service to register a client, receive callbacks from  service.
+    public static final int MSG_REGISTER_CLIENT = 1;
+
+    // Command to the service to unregister a client, stop receiving callbacks from  service.
+    public static final int MSG_UNREGISTER_CLIENT = 2;
+
+    /**
+     * Command to service to set a new value.  This can be sent to the
+     * service to supply a new value, and will be sent by the service to
+     * any registered clients with the new value.
+     */
+    public static final int MSG_SET_VALUE = 3;
+
+    /**
+     * Handler of incoming messages from clients.
+     */
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_REGISTER_CLIENT:
+                    mClients.add(msg.replyTo);
+                    break;
+                case MSG_UNREGISTER_CLIENT:
+                    mClients.remove(msg.replyTo);
+                    break;
+                case MSG_SET_VALUE:
+                    mValue = msg.arg1;
+                    for (int i = mClients.size() - 1; i >=0 ; i--) {
+                        try {
+                            mClients.get(i).send(Message.obtain(null,
+                                    MSG_SET_VALUE, mValue, 0));
+                        } catch (RemoteException e) {
+                            // Client is no longer contactable.
+                            mClients.remove(i);
+                        }
+                    }
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        mNM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+
+        // Display a notification about us starting.
+        showNotification();
+    }
+
+    @Override
+    public void onDestroy() {
+        // Cancel the persistent notification.
+        mNM.cancel(R.string.remote_service_started);
+
+        // Tell the user we stopped.
+        Toast.makeText(this, R.string.remote_service_stopped, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * When binding to the service, we return an interface to our messenger
+     * for sending messages to the service.
+     */
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mMessenger.getBinder();
+    }
+
+    /**
+     * Show a notification while this service is running.
+     */
+    private void showNotification() {
+        String NOTIFICATION_ID = "com.lukeshaun.mobileca1.service.chatservice";
+
+        // The PendingIntent to launch our activity if the user selects this notification
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, MapsActivity.class), 0);
+
+        // Get system's notification manager
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // If SDK version is Oreo or greater, use NotificationChannel to style notifications.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_ID, "Notification",
+                    NotificationManager.IMPORTANCE_HIGH);
+
+            notificationChannel.setDescription("");
+            notificationChannel.enableLights(true);
+            notificationChannel.setShowBadge(true);
+            notificationChannel.setLightColor(Color.BLUE);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.enableLights(true);
+
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        // Build notification
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_ID);
+
+        // Modify text and view of notification
+        notificationBuilder
+                .setChannelId(NOTIFICATION_ID)
+                .setContentTitle("Chat Service")
+                .setContentIntent(contentIntent)
+                .setContentText("chat service message")
+                .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setColor(Color.BLUE)
+                .setSmallIcon(R.drawable.notification)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentInfo("Info");
+
+        // Send the notification.
+        // We use a string id because it is a unique number.  We use it later to cancel.
+        mNM.notify(R.string.remote_service_started, notificationBuilder.build());
+    }
+}
